f3b2b80aab9207981f4422797ddfc5c32a5997e0
kushal-upreti <kushalupreti66@gmail.com>
2026-04-06 19:30:45 +0545

    tailwind and props basic

--- tailwind-and-props/src/App.jsx
+++ tailwind-and-props/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from './components/card'
 function App() {
   const [count, setCount] = useState(0)
+  let myObj=[1,2,3,4,5]
 
   return (
     <>
-    <h1 className='bg-green-400'>tailwind css</h1>
+  <Card username= "Kushal" />
+  <Card username= "Jason"/>
     </>
   )
 }
